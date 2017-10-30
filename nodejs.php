d7cefdb219fd75382525ad3b8c7fc6baa6d94b09
Born 2 Host <support@born2host.com>
2017-10-30 23:37:29 +0200

    Create nodejs.php

---
+++ nodejs.php
@@ -0,0 +1,30 @@
+<?php
+        if (isset($_POST['installNode'])) {
+                $setup = null;
+                $setup = $_POST['setup'];
+                $command = "curl --silent --location https://rpm.nodesource.com/setup_".$setup.".x | sudo bash - && yum -y install gcc-c++ make && y$
+                echo '<pre>';
+                echo shell_exec($command);
+                echo '</pre>';
+        }
+?>
+<pre>
+<center><h3>Install Node.js on your server</h3></center>
+</pre>
+<pre>
+<center>
+        <div class="panel-body" style="padding-bottom:0;">
+                <form method="post">
+                        <table cellpadding="5" border="0">
+                                <tr>
+                                        <td align="right">Version:</td><td align="left"><select style="width: 100px;" name="setup"><option value="6">Node.js v6</option><option value="8">Node.js v8</option></select></td>
+                                </tr>
+                                <tr>
+                                        <td></td><td align="center"><button name="installNode">Install Node.js</button></td>
+                                </tr>
+                        </table>
+                </form>
+				<center>Script by <a href="https://www.born2host.com" target="_blank">Born 2 Host</a></center>
+        </div>
+</center>
+</pre>
